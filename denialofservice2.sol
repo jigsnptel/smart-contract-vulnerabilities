3a581019ba4032adc2872a05eeffea7696d93255
Jignasha Dalal <jignasha.dalal2022@gmail.com>
2022-09-04 18:28:51 +0530

    denial of service example 2
    when a contract may iterate through an array to pay users (e.g., supporters in a crowdfunding contract). 
    It's common to want to make sure that each payment succeeds. 
    If not, one should revert. 
    The issue is that if one call fails, you are reverting the whole payout system, meaning the loop will never complete. 
    No one gets paid because one address is forcing an error.

---
+++ denialofservice2.sol
@@ -0,0 +1,12 @@
+address[] private refundAddresses;
+mapping (address => uint) public refunds;
+
+// bad
+function refundAll() public {
+    for(uint x; x < refundAddresses.length; x++) {
+ // arbitrary length iteration based on how many addresses participated
+        require(refundAddresses[x].send(refunds[refundAddresses[x]])) 
+// doubly bad, now a single failure on send will hold up all funds
+    }
+}
+
